f3215502b385eee4c588586a799733092ccc9bdc
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2017-01-07 11:41:52 +0100

    adminAuth password added

--- settings.js
+++ settings.js
@@ -102,14 +102,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$08$eUPUJQz7oHeV5myL.D9fxu2YzyWC.cElAGnahKCDtqyij9kHghmTO",
+            permissions: "*"
+        }]
+    },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
